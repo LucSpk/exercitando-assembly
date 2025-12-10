68742df73f070c94d1f818076745adcce7152b35
Lucas Alves <olalvezluc@gmail.com>
2025-12-09 21:15:14 -0300

    Cria primeira versão da atividade nv02 exercicio 2

--- exercicios_assembly/nv_02/02_ler_caracter_do_teclado/application.s
+++ exercicios_assembly/nv_02/02_ler_caracter_do_teclado/application.s
@@ -1,8 +1,24 @@
 .global _start
 .intel_syntax noprefix
 
+.section .bss
+    buffer: .skip 2
+
+.section .text
 _start:
-    
+    LEA rcx, [buffer]
+
+    MOV rax, 0 
+    MOV rdi, 0
+    LEA rsi, [buffer]
+    MOV rdx, 2
+    SYSCALL
+
+    MOV rax, 1
+    MOV rdi, 1
+    LEA rsi, [buffer]
+    MOV rdx, 2
+    SYSCALL
 
     JMP exit
 exit: 
