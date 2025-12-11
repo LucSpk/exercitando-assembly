82ede5472daff19c137ed129c6503c2f3c5cddd1
Lucas Alves <olalvezluc@gmail.com>
2025-12-11 16:32:03 -0300

    REFATORAÇÃO: Ajusta o buffer e reorganiza a lógica de comparação no código assembly

--- exercicios_assembly/nv_02/03_comparacao_e_salto_condicional/application.s
+++ exercicios_assembly/nv_02/03_comparacao_e_salto_condicional/application.s
@@ -2,14 +2,36 @@
 .intel_syntax noprefix
 
 .section .bss
-    buffer: .skip 2
+    buffer: .skip 1
 
 .section .text
 _start:
+    MOV rax, 0 
+    MOV rdi, 0
+    MOV rsi, buffer
+    MOV rdx, 1
+    SYSCALL
 
+    # debug: print primeiro byte em HEX
+    MOV al, byte ptr [buffer]
+    
+    CMP al, 'a' 
+    JNE entrada_diferente
 
+entrada_igual:
+    MOV rax, 1
+    MOV rdi, 1
+    LEA rsi, [eh_a]
+    MOV rdx, 0x06
+    SYSCALL
 
     JMP exit
+entrada_diferente:
+    MOV rax, 1
+    MOV rdi, 1
+    LEA rsi, [nao_eh_a]
+    MOV rdx, 0x0a
+    SYSCALL
 
 exit:
     MOV rax, 0x3c
