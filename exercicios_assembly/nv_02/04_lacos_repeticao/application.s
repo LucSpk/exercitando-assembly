4bfa3dbf981cd76baaec16869f64498013f665da
Lucas Alves <olalvezluc@gmail.com>
2025-12-13 16:48:56 -0300

    REFATORAÇÃO: Adiciona nova lógica para repetição e impressão de caracteres no código assembly - ainda nao funciona

--- exercicios_assembly/nv_02/04_lacos_repeticao/application.s
+++ exercicios_assembly/nv_02/04_lacos_repeticao/application.s
@@ -4,11 +4,17 @@
 .section .data
     buffer: .byte 0x30, 0x0a
 
+.section .bss
+    tmp: .byte 0
+
 .section .text
 _start: 
     LEA rcx, [buffer]
-
     CALL loop
+
+    mov cl, '0'
+    CALL loop_2
+
     JMP exit
 
 loop:
@@ -29,6 +35,28 @@ loop:
     
     cmp byte ptr [buffer], 0x39
     JL loop
+
+    RET
+
+loop_2:
+    mov [tmp], cl
+
+    MOV rax, 1
+    MOV rdi, 1
+    LEA rsi, [tmp]
+    MOV rdx, 1
+    SYSCALL
+
+    MOV byte ptr [tmp], 0x0A
+    MOV rax, 1
+    MOV rdi, 1
+    LEA rsi, [tmp]
+    MOV rdx, 1
+    SYSCALL
+
+    INC cl
+    CMP cl, '9'
+    JLE loop_2
 
     RET
 
