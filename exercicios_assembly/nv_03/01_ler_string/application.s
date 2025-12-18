4afece07564ebf4fe878a7326404a30b10a5a9bc
Lucas Alves <olalvezluc@gmail.com>
2025-12-18 18:39:27 -0300

    Sobe o codigo para receber uma string

--- exercicios_assembly/nv_03/01_ler_string/application.s
+++ exercicios_assembly/nv_03/01_ler_string/application.s
@@ -1,8 +1,27 @@
+# 01. Ler uma string
+#   Ler até 32 bytes do teclado para um buffer em .bss.
+#   Imprimir de volta.
 .global _start
 .intel_syntax noprefix
 
+.section .bss
+    buffer: .skip 32
 
+.section .text
 _start: 
+
+    MOV rax, 0x00
+    MOV rdi, 0x00
+    LEA rsi, [buffer]
+    MOV rdx, 0x20
+    SYSCALL
+
+    MOV rax, 0x01
+    MOV rdi, 0x01
+    LEA rsi, [buffer]
+    MOV rdx, 0x20
+    SYSCALL
+
     MOV rax, 0x3c
     XOR rdi, rdi
     SYSCALL
