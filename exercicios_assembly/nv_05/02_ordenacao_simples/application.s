39c198b1cff455e50fd389fddfbcf1cba0d7f089
Lucas Alves <olalvezluc@gmail.com>
2025-12-31 16:23:27 -0300

    adiciona loop com 5 voltas

--- exercicios_assembly/nv_05/02_ordenacao_simples/application.s
+++ exercicios_assembly/nv_05/02_ordenacao_simples/application.s
@@ -1,9 +1,31 @@
 .global _start
 .intel_syntax noprefix
 
+.section .bss
+    lista_leitura:      .skip 32
+    lista:              .skip 64
+    outbuf:             .skip 32
+    
+.section .text
 _start:
+    CALL .read_loop
+    JMP .exit
 
 .exit:
     MOV rax, 0x3c
     XOR rdi, rdi
     SYSCALL
+
+.read_loop:
+    MOV rcx, 0
+
+.loop:
+    CMP rcx, 5
+    JE .fim_leitura
+
+
+    INC rcx
+    JMP .loop
+
+.fim_leitura:
+    RET
