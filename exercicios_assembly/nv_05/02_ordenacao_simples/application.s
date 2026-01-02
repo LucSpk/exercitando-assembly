ca8ef5de1e2f52487220e96179c3d76c1d32a706
Lucas Alves <olalvezluc@gmail.com>
2026-01-01 23:08:38 -0300

    Cria logica para remover o newline

--- exercicios_assembly/nv_05/02_ordenacao_simples/application.s
+++ exercicios_assembly/nv_05/02_ordenacao_simples/application.s
@@ -28,7 +28,9 @@ _start:
     CMP     r12, 5
     JE      .fim_leitura
 
-    LEA     rsi, [lista_leitura]
+    # rsi recebe o local onde sera guardado a entrada 
+    # r13 o tamanho da entrada 
+    LEA     rdi, [lista_leitura]
     MOV     r13, 2
     CALL    .read_inteiro
 
@@ -40,16 +42,41 @@ _start:
     RET
 
 .read_inteiro:
-    
     PUSH    rbp
     MOV     rbp, rsp
     SUB     rsp, 32
 
+    MOV     rsi, rdi
+    MOV     rdx, r13
     MOV     rax, 0
     MOV     rdi, 0
-    # rsi deve ser atribuido na chamada da função
-    MOV     rdx, r13
     SYSCALL
 
+    LEAVE
+    RET
+
+.remove_newline:
+    PUSH    rbp
+    MOV     rbp, rsi
+    SUB     rsp, 16
+
+    MOV     rcx, 0
+
+.loop_remove_newline:
+    # Vai percorrer a entrada até encontrar o '\n'
+    MOV     al, byte ptr [rdi + rcx]        # Move um byte para 'al' usando a contagem em rcx como cursor
+    CMP     al, 0x0a                        # Verifica se é igual a '\n'
+    JE      .replace_newline
+
+    CMP     al, 0x00                        # Verifica se chegou ao fim (evitar o loop infinito caso não haja um '\n')
+    JE      .fim_remove_newline
+
+    INC     rcx
+    JMP     .loop_remove_newline
+
+.replace_newline:
+    MOV     byte ptr [rdi + rcx], 0x00      # Subistitui o '\n' por um caracter null
+
+.fim_remove_newline:
     LEAVE
     RET
